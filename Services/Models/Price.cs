8156b970e9dd6a079865ddb9e8d645c76871a3b6
Nicolas Barbery <nicobarbery@outlook.es>
2021-05-11 10:57:23 -0400

    QUO-003: Added real PricingBooks and Added Four quotes for executing tests at the Json file

--- Services/Models/Price.cs
+++ Services/Models/Price.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using UPB.FinalProject.Data.Models;
 
 namespace UPB.FinalProject.Services.Models
 {
     public class Price
     {
         public string CodProd { get; set; }
-        public double SetPrice { get; set; }
-
-        public double PromotionPrice { get; set; }
+        public string ProdName { get; set; }
+        public string CodDesc { get; set; }
+        public List<Quotation> Products { get; set; }
 
     }
 }
